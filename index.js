af58f02b1c69a22ae9365d67bda5b7b4d5d0ffd1
regenerik <regenerik@gmail.com>
2022-09-04 17:50:54 -0300

    port

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const axios  = require("axios")
 const {scraping} = require("./src/generalControllerss.js")
+const  port = 3001 || PORT.env
 
 // sync todos los modelos de una vez;
 
 
   
   conn.sync({ force: true }).then(() => {
-    server.listen(3001, () => {
+    server.listen({port}, () => {
     console.log('%s listening at 3001🥂')
     scraping()
     });
